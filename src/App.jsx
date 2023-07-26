6f9de53ec22a3846dd54ee58778e2fab65341cda
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-26 14:10:37 +0500

    Setting Up Error page for undefined route

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <nav>Our Navbar</nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="products" element={<Products />} />
         <Route path="about" element={<About />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="login" element={<Login />} />
+        <Route path="*" element={<Error />} />
       </Routes>
+      <footer>Our Footer</footer>
     </BrowserRouter>
   );
 };
