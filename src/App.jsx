9ba7f5c02a62ed227ade8494a6ab4d65f3510104
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-26 12:30:00 +0500

    Setting Up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <section className='section'>
-      <h2>Home Page</h2>
+    <section className="section">
+      <h2>HomePage</h2>
+      <Link to="about">AboutUs</Link>
+      <Link to="dashboard">Dashboard </Link>
+      <Link to="login">Login</Link>
+      <Link to="products">Products</Link>
     </section>
   );
 };
